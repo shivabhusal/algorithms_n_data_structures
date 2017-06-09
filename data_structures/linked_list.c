f8728c7b28e7c7e179f4cf39c42626f4225e34cf
Shiva Bhusal <hotline.shiva@gmail.com>
2017-06-09 22:43:06 +0545

    Singly linked list implemented - with methods/operations 'append', 'delete'

---
+++ data_structures/linked_list.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct Node{
+  int    data;
+  struct Node* next;
+};
+
+//void prepend(Node* child, int data){
+//
+//}
+
+
+struct Node* append(struct Node* parent, int sperm){
+  struct Node* child = malloc(sizeof(struct Node));
+
+  child ->data = sperm;
+  parent->next = child;
+
+  // After 9 months
+  return child;
+}
+
+
+struct Node* findParentOf(struct Node* headNode, struct Node* targetNode){
+  struct Node* tempPointer = headNode;
+  while(tempPointer->next != targetNode){
+    tempPointer = tempPointer->next;
+  }
+  return tempPointer;
+}
+
+//void retrieve();
+void delete(struct Node* headNode, struct Node* targetNode){
+  struct Node* parentNode = findParentOf(headNode, targetNode);
+
+  // Establishing relnship between the nearby nodes.
+  parentNode->next = targetNode->next;
+
+  // Deleting the object to free the memory
+  free(targetNode);
+}
+
+
+void testRunner();
+
+void main(){
+  testRunner();
+}
+
+void testRunner(){
+  struct Node headNode = {.data=12};
+  assert(headNode.data == 12);
+
+  struct Node* node1 = append(&headNode, 13);
+  assert(node1->data == 13);
+  assert(headNode.next   == node1);
+
+  struct Node* node2 = append(node1, 14);
+  assert(node2->data == 14);
+  assert(node1->next == node2);
+
+  // Delete Node1
+  assert(findParentOf(&headNode, node1) == &headNode);
+  assert(findParentOf(&headNode, node2) == node1);
+
+  delete(&headNode, node1);
+  assert(node1->data != 13);
+  assert(headNode.next == node2);
+
+}
+
